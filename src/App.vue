ad94a4c92431644f3f909463c777d4e2dad92e72
KuroiMatoO <kuroimato.o@gmail.com>
2024-07-29 00:43:40 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+export default{
+    data(){
+        return {
+            message: 'Click me!'
+        }
+    },
+    methods: {
+        changeMessage(){
+            this.message = 'Hello World!'
+        }
+    }
+}
+</script>
+
+<template>
+    <div className="wrapper" @click="changeMessage()">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<style scoped>
+    .wrapper{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: rgb(63, 63, 63);
+        width: 30vw;
+        height: 20vw;
+        color: rgb(199, 199, 199);
+        font-size: 3vw;
+        border-radius: 15px;
+        cursor: pointer;
+        transition: transform 0.1s ease;
+    }
+    .wrapper:active{
+        transform: translateY(2px);
+    }
+    .wrapper p{
+        user-select: none;
+    }
+</style>
