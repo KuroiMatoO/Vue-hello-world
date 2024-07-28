01e4359ab4d7e1138aa908bd96d2e7c0d5ef493e
KuroiMatoO <kuroimato.o@gmail.com>
2024-07-29 00:52:17 +0300

    dist-update

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: './', // This will ensure that the asset paths are relative to the current directory
+  build: {
+    assetsDir: '', // This will place the generated assets in the same directory as the build output
+  },
 })
